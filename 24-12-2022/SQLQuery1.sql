1dbfc74f437ac2602e0bdee354969434a59b4a1d
Rajkumar K <k.rajkumarmca@gmail.com>
2023-01-03 17:33:51 +0530

    KRK

--- 24-12-2022/SQLQuery1.sql
+++
@@ -1,6 +0,0 @@
-select  empcode
-,present,leave,absent,weeklyoff,totaldays,paid_days
-,basic,attnearning,hraamount1,hraamount
-,washamount1,washamount 
-,daamount1,daamount,rsperday
-,* from tmpSalProcess
